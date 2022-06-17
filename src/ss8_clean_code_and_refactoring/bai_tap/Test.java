018a795365745d17e1f717600f0c1445f863d239
dntrung1 <dntrung1@gmail.com>
2022-06-17 08:28:27 +0700

    update

---
+++ src/ss8_clean_code_and_refactoring/bai_tap/Test.java
@@ -0,0 +1,15 @@
+//package ss8_clean_code_and_refactoring.bai_tap;
+//
+//public class Test {
+////    public static String getScore(String player1Name, String player2Name, int scoreOfPLayre1, int scoreOfPlayer2){
+////        String score = "";
+////        int tempScore=0;
+////
+////    }
+//    public Test testScore(int scoreOfPlayer1,int scoreOfPlayer2){
+//        if (scoreOfPlayer1==scoreOfPlayer2){
+//
+//        }
+//
+//    }
+//}
